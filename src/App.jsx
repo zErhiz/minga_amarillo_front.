d7d49d6593e599cf43b11e04a842cce64ce51b58
Mateo Carrizo FLores <127887470+zErhiz@users.noreply.github.com>
2023-04-24 14:00:12 -0300

    F 01 (#3)
    * segundocommit
    
    * responsive
    
    * responsive full
    
    * f-01commit
    
    * f-01
    
    * f-01 git ignore

--- src/App.jsx
+++ src/App.jsx
@@ -26,17 +26,21 @@ function App() {
   return (
     <>
 
+
     <header className="  bg-black  bg-hero bg-[38%] bg-cover   h-[100vh]  w-[100%] sm:h-[50vh] xl:h-[60vh] ">
       <nav className='flex justify-center  '>
         <div className="  flex items-center justify-between w-5/6  xl:mt-10">
         <img className='sm:h-8 sm:mt-3 xl:h-12' src ={imagenmenu} alt="" />
         <div className='sm:flex  sm:items-center sm:mt-8 sm:gap-4  sm:flex-row'> 
+
         <h2 className='hidden sm:block text-orange-500 text-2xl xl:text-4xl'>Minga</h2>
         <img className='sm:h-6 xl:h-8' src={ultima} alt="" />
         </div>
         </div>
       </nav>
       <div className=' h-[100%] flex justify-center items-center sm:h-[82%] ' > 
+
+
       <div className='mb-40 w-scren items-center flex flex-col   sm:mb-20 sm:w-5/6 sm:items-start'> 
 
         <h1 className='text-white font-bold text-3xl w-[80%]  text-center  sm:w-[70%] sm:text-left xl:text-6xl'>For the love of  manga</h1>
@@ -51,7 +55,10 @@ function App() {
     </header>
 
 
+
+
     <main className="hidden bg-white sm:block sm:p-10 lg:p-28 xl:p-12"> 
+
     <div className="bg-bgnaranja bg-cover flex sm:flex-row justify-between items-center gap-4 lg:gap-20">
   <div className="flex sm:w-1/4 items-center justify-center">
     <img src={flechaizq} alt="" className="w-8 h-8 flex justify-start" />
