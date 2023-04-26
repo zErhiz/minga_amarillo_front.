d284790a84cf8595ee582fbac5c79066a5022b65
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-04-26 09:48:37 -0300

    carousel fix

--- src/assets/components/Carousel.jsx
+++ src/assets/components/Carousel.jsx
@@ -15,46 +15,64 @@ export default function Carousel() {
   
 
   
- let [categories, setCategories] = useState ([])
- console.log(categories)
-  let [counter, setCounter] = useState(0)
-  console.log(counter)
-  let sumar = ()=> {
-    console.log(counter)
-    setCounter(counter+1)
-    if(counter ==categories.length-1)
-    {
-      setCounter(0)
-    }
+  const [categories, setCategories] = useState([])
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+  
+    const valorInterval = setInterval(() => {
+      setCounter(prevCounter => {
+        if (prevCounter === categories.length - 1) {
+          return 0
+        } else {
+          return prevCounter + 1
+        }
+      })
+    }, 4000)
+    
+    console.log(valorInterval)
+    
+    return () => clearInterval(valorInterval)     //detiene el intervalo y evita que se siga inecesariamente
+  }, [categories.length])         
+  const arrowPrev = () => {
+    setCounter(prevCounter => {
+      if (prevCounter === 0) {
+        return categories.length - 1
+      } else {
+        return prevCounter - 1
+      }
+    })
   }
-  let restar =()=>
-  {
-    console.log(counter)
-    setCounter(counter-1)
-    if(counter===0)
-    {
-      setCounter(categories.length-1)
-    }
+
+  const arrowAfter = () => {
+    setCounter(prevCounter => {
+      if (prevCounter === categories.length - 1) {
+        return 0
+      } else {
+        return prevCounter + 1
+      }
+    })
   }
+
   //el hook puede ir aca pero lo correcto es en las primeras lineas
   return (
-    <>  <main className="hidden bg-white sm:block sm:p-10 lg:p-28 xl:p-12 border border-black"> 
+    <>  <main className="hidden bg-white sm:block sm:p-10 lg:p-28 xl:p-12 "> 
   
-    <div className="  flex sm:flex-row justify-between items-center gap-4 lg:gap-20 border border-black"style={{backgroundColor:categories[counter]?.color}}>
-  <div className="flex sm:w-1/4 items-center justify-center border border-black">
-    <img onClick={restar}src={flechaizq} alt="" className="w-8 h-8 flex justify-start border border-black  cursor-pointer" />
+    <div className="  flex sm:flex-row justify-between items-center gap-4 lg:gap-20 "style={{backgroundColor:categories[counter]?.color}}>
+  <div className="flex sm:w-1/4 items-center justify-center  ">
+    <img onClick={arrowPrev}src={flechaizq} alt="" className="w-8 h-8 flex justify-start  cursor-pointer" />
   </div>
-  <div className="flex sm:w-1/2 items-center justify-center lg:gap-20 xl:gap-40 border border-black">
-    <img src={categories[counter]?.character_photo} alt="nami" className=" sm:h-50 sm:ml-4 sm:mt-[-1rem] lg:mt-[-5rem]  xl:h-60 xl:ml-[-9rem] xl:mt-[-4rem] border border-black" />
-    <img src={categories[counter]?.cover_photo} alt="luffy" className="sm:h-50 sm:mt-[-2rem] xl:h-60 lg:mt-[-5rem]  xl:ml-[-2rem] xl:mt-[-4rem] border border-black" />
+  <div className="flex sm:w-1/2 items-center justify-center lg:gap-20 xl:gap-40 ">
+    <img src={categories[counter]?.character_photo} alt="nami" className=" sm:h-50 sm:ml-4 sm:mt-[-1rem] lg:mt-[-5rem]  xl:h-60 xl:ml-[-9rem] xl:mt-[-4rem] " />
+    <img src={categories[counter]?.cover_photo} alt="luffy" className="sm:h-50 sm:mt-[-2rem] xl:h-60 lg:mt-[-5rem]  xl:ml-[-2rem] xl:mt-[-4rem] " />
   </div>
-  <div className="flex sm:w-1/4 items-center justify-center xl:w-0 border border-black"></div>
-  <div className="w-full sm:w-1/2  xl:mt-[4rem] border border-black">
-    <h3 className="text-white sm:text-2xl font-semibold border border-black">{`${categories[counter]?.name[0].toUpperCase()}${categories[counter]?.name.substring(1)}`}</h3>
-    <p className="text-white sm:text-1xl sm:overflow-auto sm:h-20  xl:h-40 border border-black">{categories[counter]?.description}</p>
+  <div className="flex sm:w-1/4 items-center justify-center xl:w-0 "></div>
+  <div className="w-full sm:w-1/2  xl:mt-[4rem] ">
+    <h3 className="text-white sm:text-2xl font-semibold ">{`${categories[counter]?.name[0].toUpperCase()}${categories[counter]?.name.substring(1)}`}</h3>
+    <p className="text-white sm:text-1xl sm:overflow-auto sm:h-20  xl:h-40 ">{categories[counter]?.description}</p>
   </div>
-  <div className="flex sm:w-1/4 items-center justify-center border border-black">
-    <img onClick={sumar} src={flechader} alt="" className="w-8 h-8 border border-black cursor-pointer" />
+  <div className="flex sm:w-1/4 items-center justify-center ">
+    <img onClick={arrowAfter} src={flechader} alt="" className="w-8 h-8  cursor-pointer" />
   </div>
 </div>
 </main>
