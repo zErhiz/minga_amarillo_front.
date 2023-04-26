7bbf08e77bb3bb32bc6f5c97c88e41d11a68c727
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-04-26 11:45:20 -0300

    fix

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 let apiUrl = 'https://minga-amarillo-back-xeji.onrender.com/'
 
 if (process.env.NODE_ENV==='production') {
-    apiUrl = import.meta.env.VITE_URL
+    apiUrl = import.meta.env.VITE_API
 }
 
 export default apiUrl
