004995f9e81aa40ef27870995e365a549a63f6c7
unknown <cris.sandoval.cf@gmail.com>
2023-04-30 16:48:51 -0300

    form-ok

--- src/assets/components/ChapterForm.jsx
+++ src/assets/components/ChapterForm.jsx
@@ -25,12 +25,12 @@ function handleForm(e){
 }
   return (
     <>
-    <form onSubmit={(e)=>handleForm(e)} action="" className="w-fukk h-screen flex flex-col justify-center px-28 gap-10 items-center">
-      <p className="text-[34px] min-[320px]:w-[65vw]">New Chapter</p>
-      <input className="border-b-slate-800 border py-2 px-4 rounded w-[20%] min-[320px]:w-[65vw]" type="text" placeholder="Insert title" ref={title} id='title'/>
-      <input className="border-b-slate-800 border py-2 px-4 rounded w-[20%] min-[320px]:w-[65vw]" type="text" placeholder="Insert order" ref={order} id='order'/>
-      <input className="border-b-slate-800 border py-2 px-4 rounded w-[20%] min-[320px]:w-[65vw]" type="text" placeholder="Insert pages" ref={pages} id='pages'/>
-      <input className=" py-2 px-4 rounded-full bg-orange-500 w-[15%] text-white min-[320px]:w-[55vw]" type="submit"  value="send"/>
+    <form onSubmit={(e)=>handleForm(e)} action="" className="w-full h-screen flex flex-col justify-center px-28 gap-12 items-center">
+      <p className="text-[34px] min-[320px]:w-[65vw] sm:text-[55px] text-center">New Chapter</p>
+      <input className="border-b-slate-800 border py-2 px-4 rounded w-[20%] min-[320px]:w-[65vw] md:h-[70px]" type="text" placeholder="Insert title" ref={title} id='title'/>
+      <input className="border-b-slate-800 border py-2 px-4 rounded w-[20%] min-[320px]:w-[65vw] md:h-[70px]" type="text" placeholder="Insert order" ref={order} id='order'/>
+      <input className="border-b-slate-800 border py-2 px-4 rounded w-[20%] min-[320px]:w-[65vw] md:h-[70px]" type="text" placeholder="Insert pages" ref={pages} id='pages'/>
+      <input className=" py-2 px-4 rounded-full bg-orange-500 w-[15%] text-white min-[320px]:w-[55vw] md:h-[70px]" type="submit"  value="send"/>
     </form>
     </>
   )
