c7876f5944ec2d7993543c8be1ac5aab7c9b7ae5
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-05-03 19:37:05 -0300

    dev fix

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,16 +5,33 @@ import Main from '../../src/App'
 import Layout from '../../src/assets/layouts/Main'
 import Authorform from '../assets/pages/Authorform'
 import ChapterForm from "../assets/components/ChapterForm.jsx";
+import SignUp from '../../src/assets/pages/SignUp'
+import  Login  from '../../src/assets/pages/Login'
 let user = JSON.parse(localStorage.getItem('user'));
 let role = user ? user.role : null;
 const routes = createBrowserRouter(
     [
         { path: '/', element:<Layout/>, children:
         [
-              {path:'/mangas-form',element: <MangaForm/> }
+              {path:'/mangas-form',element: <MangaForm/> },
             {path:'/', element:<Main/> },
-            {path:'/author-form', element:<Authorform/>}
-            {path: "/chapters/:id", element: ( role === 1||role === 2 ?  <ChapterForm/> : <Main/>  )}
+            {path:'/author-form', element:<Authorform/>},
+            {path: "/chapters/:id", element: ( role === 1||role === 2 ?  <ChapterForm/> : <Main/>  )},
+            {
+                path:'/', element:<Main/>
+            },
+            {
+                path:'/register', element:<SignUp/>
+            },
+          {
+            path:'/login', element:<Login/>
+          },
+          {
+            path:'/home', element:<Main/>
+          },
+          {
+            path:'/auth', element:<Login/>
+          },
         ]}
     ])
     export default routes
