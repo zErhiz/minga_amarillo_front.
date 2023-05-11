2e4b9baffe5275cf75917492bb415610f76880a0
FranciscoPedicino <franciscopedi91@gmail.com>
2023-05-11 02:43:07 -0300

    fixed

--- src/store/reducers/chapter_one.js
+++ src/store/reducers/chapter_one.js
@@ -1,23 +1,32 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createReducer } from "@reduxjs/toolkit";
+//importo las acciones
+import chapter_action from "../actions/chapter_one";
 
-//la accion es un intermediario entre la vista y las operaciones de reduccion!
-//es la que DISPARA/EJECUTA la modificacion/reduccion de los estados globales
-const chapter_one = createAction(
-  'chapter_one', //nombre de la accion
-  (objeto) => {    //funcion que va a enviar datos al reductor 
-                  //el objeto debe tener todas las propiedades a guardarse en el estado global
-    return {
-      payload: {
-        title: objeto.title,
-        order:objeto.order,
-        cover_photo: objeto.cover_photo,
+//desestructuro la accion para poder utilizarla
+const {chapter_one} = chapter_action
+
+//defino estado inicial
+let initial_state = {
+  title: '',
+  order:'',
+  cover_photo: '',
+}
+
+const reducer = createReducer (
+  initial_state,
+  (builder) => builder
+  .addCase(
+    chapter_one,
+    (state, action) => {
+      const new_state = {
+        ...state, 
+        title: action.payload.title,
+        cover_photo: action.payload.cover_photo,
+       order:action.payload.order,
       }
+      return new_state
     }
-  } 
+  )
 )
 
-//la accion no tiene demasiada logica xq su unico objetivo es enviar informacion al reductor. 
-//Y en este mismo se realiza TODA la logica necesaria para modificar/reducir los estados globales.
-const actions = {chapter_one}
-//construyo un objeto con la accion (mas adelante se van a agregar mas acciones)
-export default actions
+export default reducer
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import manga_reducer from './reducers/manga_one'
-import chapter_reducer from './reducers/chapter_one'
+import manga_reducer from './reducers/manga_one.js'
+import chapter_reducer from './reducers/chapter_one.js'
 
 
 const store = configureStore({
-  reducer: {
+  reducer:{
     chapter:chapter_reducer,
     manga:manga_reducer,
 
