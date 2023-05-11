f3f33a41a7ccb352d55f012857a4ddd3505d080e
Mateo Carrizo FLores <127887470+zErhiz@users.noreply.github.com>
2023-05-11 08:48:26 -0300

    falta reeducx (#14)
    * falta reeducx
    
    * redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routes from '../src/router/router.jsx'
+import { Provider } from 'react-redux'
+import store from './store/store.jsx' 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <RouterProvider router={routes} />
-
+    <Provider store ={store}>
+        <RouterProvider router={routes} />
+    </Provider>,
 )
+//el enrutador tiene que ser hgijo del almacenamient ode estados globales para que 
+//cada interfaz del enrutador pueda acceder a los estados globales de redux
