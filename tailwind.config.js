9fdd2fe545a308fbf6d3d550691d2a1ff8196a6c
Mateo Carrizo FLores <127887470+zErhiz@users.noreply.github.com>
2023-05-15 17:13:32 -0300

    m-12 mitad completa (#21)

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
     backgroundImage :{
       'hero': "url('../public/img/Images + Rectangle.png')",
       'bgnaranja': "url('../public/img/rectangulonaranja.png')",
-
+      'bgnewrole': "url('../public/img/newroleImage.png')",
       'bgform':"url('../public/img/BGFORM.png')",
       'bgform2':"url('../public/img/BGFORM2.PNG')",
       'bgmanga':"url('../public/img/bgmanga.png')",
