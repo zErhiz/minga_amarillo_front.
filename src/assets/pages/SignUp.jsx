a8f24d172d1c34bd850d7fd18520483d6244bbc5
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-05-29 16:20:20 -0300

    pck

--- src/assets/pages/SignUp.jsx
+++ src/assets/pages/SignUp.jsx
@@ -10,9 +10,10 @@ import Swal from 'sweetalert2'
  import { gapi } from "gapi-script"
 import {GoogleLogin} from "react-google-login"
  import Error from '../components/Error'
-
+ import { current } from '@reduxjs/toolkit'
 export const SignUp = () => {
   const navigate = useNavigate()
+  const clientID = "166461268192-b8ojdp58ns00djbekuc8o7p2p7lm5krb.apps.googleusercontent.com"
   
   const password = useRef()
   const email= useRef()
@@ -61,7 +62,6 @@ export const SignUp = () => {
     })
     
   }
-  const clientID = "166461268192-b8ojdp58ns00djbekuc8o7p2p7lm5krb.apps.googleusercontent.com"
 
   useEffect(()=> {
     const start = () => {
@@ -148,13 +148,13 @@ export const SignUp = () => {
           </div>
         
        <div className=' flex h-[80%] justify-center '>
-       <form  onSubmit={(e)=>handleForm(e)} className='w-[80%] h-full bg-white text-orange-500 flex flex-col px gap-2'>
+       <form  onSubmit={(e)=>handleForm(e)} className='w-[80%] h-full bg-white text-orange-500 flex flex-col px gap-2'  encType="multipart/form-data" method="post">
         <label htmlFor="">Email</label>
-        <input className='border border-black px-4 py-2 rounded-md' type="text" placeholder='email' ref={email} />
+        <input className='border border-black px-4 py-2 rounded-md' type='email' placeholder='email' ref={email} />
         <label htmlFor="">Password</label>
         <input className='border border-black px-4 py-2 rounded-md' type="text" placeholder='password' ref={password}/>
         <label htmlFor="">Photo</label>
-        <input  className='border border-black px-4 py-2 rounded-md' type="text" placeholder='url' ref={photo}/>
+        <input name='photo'  className='peer block min-h-[auto] w-full rounded border-0 bg-neutral-100 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:bg-neutral-700 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' type="file" placeholder='url' ref={photo}/>
         <input className='bg-orange-400 border border-black py-4 text-white text-center rounded-md font-semibold hover:bg-white hover:text-orange-600 cursor-pointer' type="submit" value='Sign Up' />
         <GoogleLogin
             clientId = {clientID}
@@ -162,7 +162,8 @@ export const SignUp = () => {
             onSuccess={onSuccess}
             onFailure={onFailure}
             cookiePolicy={'single_host_origin'}  />
-        </form>
+    
+            </form>
       
        </div>
         
