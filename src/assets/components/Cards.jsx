32e5721f0720242377b2719e5ffde72b9fe0afbc
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-06-21 17:16:39 -0300

    Fixed

--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Cards({title, img, props}) {
   return (
-  <div className='w-screen lg:w-1/2  p-4'>
-    <div className='bg-white  flex justify-between items-center border sm:w-[70%] rounded-xl  sm:h-52 '>
+  <div className='w-[100%] lg:w-[50%] xl:w-[35%]  p-4 '>
+    <div className='bg-white  flex justify-between items-center border sm:w-[100%] rounded-xl h-40  2xl: sm:h-52 '>
+       
         <div className='flex  gap-6 flex-col  p-6'>
         <h2>{title}</h2>
         <div>
@@ -12,7 +13,9 @@ export default function Cards({title, img, props}) {
         className='bg-green-400 p-2 rounded-3xl font-semibold text-sm text-green-700'>Read</button>
         </div>
         </div>
-        <img className='min-h-[21vh] max-h-[21vh] min-w-[45%] max-w-[45%] object-cover rounded-[50%_50%_48%52%/100%_0%_0%_100%]' src={img} alt="" />
+        <div className=' flex justify-end h-[100%] w-[45%]'> 
+        <img className='h-[100%] w-[100%] object-cover'  style={{ borderTopLeftRadius: 1000,borderBottomLeftRadius: 1000}} src={img} alt="" />
+        </div>
     </div>
     </div>
   )
